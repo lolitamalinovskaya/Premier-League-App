f8780b48ccd13b837643ce066f2c5365d297464a
lolitaMalinovskaya <lolita.malinovskaya@wedextim.com>
2022-03-30 18:43:20 +0300

    Add variable for navigation className

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,30 +8,32 @@ const Navbar = () => {
 
     const toggleNavBar = () => setHhowNavBar(!showNavBar);
 
+    const navClassName = showNavBar ? "navbar-show" : "navbar-hide";
+
     return (
         <div className="navbar">
             <li onClick={toggleNavBar}>
                 <img className="navbar-logo" src={Logo} alt="Логотип"/>
             </li>
-            <li onClick={toggleNavBar} className={showNavBar ? "navbar-show" : "navbar-hide"}>
+            <li onClick={toggleNavBar} className={navClassName}>
                 <Link to="/">Home</Link>
             </li>
-            <li onClick={toggleNavBar} className={showNavBar ? "navbar-show" : "navbar-hide"}>
+            <li onClick={toggleNavBar} className={navClassName}>
                 <Link to="/players">Players</Link>
             </li>
-            <li onClick={toggleNavBar} className={showNavBar ? "navbar-show" : "navbar-hide"}>
+            <li onClick={toggleNavBar} className={navClassName}>
                 <Link to="/teams">Teams</Link>
             </li>
-            <li onClick={toggleNavBar} className={showNavBar ? "navbar-show" : "navbar-hide"}>
+            <li onClick={toggleNavBar} className={navClassName}>
                 <Link to="/matches">Matches</Link>
             </li>
-            <li onClick={toggleNavBar} className={showNavBar ? "navbar-show" : "navbar-hide"}>
+            <li onClick={toggleNavBar} className={navClassName}>
                 <Link to="/table">Table</Link>
             </li>
-            <li onClick={toggleNavBar} className={showNavBar ? "navbar-show" : "navbar-hide"}>
+            <li onClick={toggleNavBar} className={navClassName}>
                 <Link to="/logIn">LogIn</Link>
             </li>
-            <li onClick={toggleNavBar} className={showNavBar ? "navbar-show" : "navbar-hide"}>
+            <li onClick={toggleNavBar} className={navClassName}>
                 <Link to="/signUp">SignUp</Link>
             </li>
         </div>
