5347f949c92f4a647ea77edc974ec6be16f49f3c
user <user@macbook.pro>
2022-05-04 21:57:08 +0300

    fix safari date parsing

--- src/components/parseDate.js
+++ src/components/parseDate.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const parseDate = string => {
-    let fullDate = new Date(string);
+    let fullDate = new Date(string.replace(/-/g, "/"));
     let month = fullDate.toLocaleString('en', {month: "short"});
     let year = fullDate.getFullYear();
     let time = `${fullDate.getHours()}:${fullDate.getMinutes()}`;
